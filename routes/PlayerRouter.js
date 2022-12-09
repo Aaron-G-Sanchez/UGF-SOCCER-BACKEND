61c6e39950ba8516831e6e6df4bf1dde8ca17fed
Aaron Sanchez <saarong12@gmail.com>
2022-12-09 13:43:50 -0600

    Middleware set up on player router

--- routes/PlayerRouter.js
+++ routes/PlayerRouter.js
@@ -1,8 +1,14 @@
 const { Router } = require('express')
 const controllers = require('../controllers/PlayerController')
+const middleware = require('../middleware')
 
 const router = Router()
 
-router.get('/', controllers.GetAllPlayers)
+router.get(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controllers.GetAllPlayers
+)
 
 module.exports = router
