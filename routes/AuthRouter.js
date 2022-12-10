736358c5ce88c37b0963b4ea4fa6c846f03e00ec
Aaron Sanchez <saarong12@gmail.com>
2022-12-10 10:05:43 -0600

    Check session route and controller set up

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,9 +1,17 @@
 const { Router } = require('express')
 const controllers = require('../controllers/AuthController')
+const middleware = require('../middleware')
 
 const router = Router()
 
 router.post('/login', controllers.Login)
 router.post('/register', controllers.Register)
+
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controllers.CheckSession
+)
 
 module.exports = router
